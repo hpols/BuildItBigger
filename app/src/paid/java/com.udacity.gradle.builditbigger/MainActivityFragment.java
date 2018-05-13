358f4248c33d590989790ff0bdc47a9c759931d4
HildePols <atschpe@gmail.com>
2018-05-13 16:20:52 +0200

    flavors added and cleaned

--- app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
@@ -12,8 +12,6 @@ import android.widget.Button;
 
 import com.example.android.androidlibrary.JokerActivity;
 import com.example.android.javalib.Joker;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -31,15 +29,6 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
         Context context = getActivity();
-
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
 
         jokeBt = root.findViewById(R.id.joke_bt);
         jokeBt.setOnClickListener(new View.OnClickListener() {
